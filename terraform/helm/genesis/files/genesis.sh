317f6e8ddd9d2068f622c5ff76cb12e5ed626292
Rustie Lin <rustie117@gmail.com>
2022-06-17 22:45:14 -0700

    [helm][genesis] do genesis in an untemplated script

--- terraform/helm/genesis/files/genesis.sh
+++ terraform/helm/genesis/files/genesis.sh
@@ -4,15 +4,30 @@
 # Runs an automated genesis ceremony for validators spun up by the aptos-node helm chart
 #
 # Expect the following environment variables to be set before execution:
-# WORKSPACE
 # NUM_VALIDATORS
-# USERNAME_PREFIX
+# ERA
+# WORKSPACE: default /tmp
+# USERNAME_PREFIX: default aptos-node
 # VALIDATOR_HOST_SUFFIX: default validator-lb
 # FULLNODE_HOST_SUFFIX: default fullnode-lb
 # 
 
+WORKSPACE=${WORKSPACE:-/tmp}
+USERNAME_PREFIX=${USERNAME_PREFIX:-aptos-node}
 VALIDATOR_HOST_SUFFIX=${VALIDATOR_HOST_SUFFIX:-validator-lb}
 FULLNODE_HOST_SUFFIX=${FULLNODE_HOST_SUFFIX:-fullnode-lb}
+
+if [[ -z ${ERA} ]] || [[ -z ${NUM_VALIDATORS} ]]; then
+    echo "ERA and NUM_VALIDATORS must be set"
+    exit 1
+fi
+
+echo "NUM_VALIDATORS=${NUM_VALIDATORS}"
+echo "ERA=${ERA}"
+echo "WORKSPACE=${WORKSPACE}"
+echo "USERNAME_PREFIX=${USERNAME_PREFIX}"
+echo "VALIDATOR_HOST_SUFFIX=${VALIDATOR_HOST_SUFFIX}"
+echo "FULLNODE_HOST_SUFFIX=${FULLNODE_HOST_SUFFIX}"
 
 # generate all validator configurations
 for i in $(seq 0 $(($NUM_VALIDATORS-1))); do
@@ -20,12 +35,11 @@ username="${USERNAME_PREFIX}-${i}"
 user_dir="${WORKSPACE}/${username}"
 mkdir $user_dir
 aptos genesis generate-keys --output-dir $user_dir
-aptos genesis set-validator-configuration --keys-dir $user_dir --local-repository-dir ${WORKSPACE} \
+aptos genesis set-validator-configuration --keys-dir $user_dir --local-repository-dir $WORKSPACE \
     --username $username \
     --validator-host "${username}-${VALIDATOR_HOST_SUFFIX}:6180" \
     --full-node-host "${username}-${FULLNODE_HOST_SUFFIX}:6182"
 done
-
 
 # get the framework
 # this is the directory the aptos-framework is located in the aptoslabs/init docker image
@@ -35,13 +49,16 @@ cp -R $FRAMEWORK_DIR ${WORKSPACE}/framework
 # run genesis
 aptos genesis generate-genesis --local-repository-dir ${WORKSPACE} --output-dir ${WORKSPACE}
 
-# TODO(rustielin): delete old genesis artifacts
+# delete all fullnode storage except for those from this era
+kubectl get pvc -o name | grep /fn- | grep -v "e${ERA}-" | xargs -r kubectl delete
+# delete all genesis secrets except for those from this era
+kubectl get secret -o name | grep "genesis-e" | grep -v "e${ERA}-" | xargs -r kubectl delete
 
-# create genesis for validators to startup
+# create genesis secrets for validators to startup
 for i in $(seq 0 $(($NUM_VALIDATORS-1))); do
 username="${USERNAME_PREFIX}-${i}"
 user_dir="${WORKSPACE}/${username}"
-kubectl create secret generic {{ $user.name }}-genesis-e{{ $.Values.chain.era }} \
+kubectl create secret generic "${username}-genesis-e${ERA}" \
     --from-file=genesis.blob=${WORKSPACE}/genesis.blob \
     --from-file=waypoint.txt=${WORKSPACE}/waypoint.txt \
     --from-file=validator-identity.yaml=${user_dir}/validator-identity.yaml \
